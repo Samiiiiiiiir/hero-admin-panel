cea47b556bd2002a55060bd40b20423a163d09b6
Samir <sam1r.aslanoff.as@gmail.com>
2025-02-18 20:51:43 +0200

    changed hook a little

--- src/hooks/http.hook.js
+++ src/hooks/http.hook.js
@@ -2,7 +2,6 @@ import { useCallback } from 'react';
 
 export const useHttp = () => {
   // const [process, setProcess] = useState('waiting');
-
   const request = useCallback(
     async (
       url,
@@ -11,16 +10,12 @@ export const useHttp = () => {
       headers = { 'Content-Type': 'application/json' }
     ) => {
       // setProcess('loading');
-
       try {
         const response = await fetch(url, { method, body, headers });
-
         if (!response.ok) {
           throw new Error(`Could not fetch ${url}, status: ${response.status}`);
         }
-
         const data = await response.json();
-
         return data;
       } catch (e) {
         // setProcess('error');
@@ -29,11 +24,9 @@ export const useHttp = () => {
     },
     []
   );
-
   // const clearError = useCallback(() => {
   // setProcess('loading');
   // }, []);
-
   return {
     request,
     // clearError,
